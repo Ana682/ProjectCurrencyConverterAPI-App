9ba4447947cefeeaa61b42be4cb9d7006800ee7d
Ana682 <queiroz.silva.ana@escola.pr.gov.br>
2025-08-20 09:34:42 -0300

    Crie arquivos para funcionamento da aplicação

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -1,4 +1,5 @@
 //Modelo de dados (DTO - Data Transfer Object) que representa a resposta json da API
+//Json
 package com.example.projectcurrencyconverterapi;
 
 public class ExchangeRatesResponse {
---
+++ app/src/main/java/com/example/projectcurrencyconverterapi/RetrofitClient.java
@@ -0,0 +1,5 @@
+//Classe responsável por configurar e fornecer uma instancia retrofit
+package com.example.projectcurrencyconverterapi;
+
+public class RetrofitClient {
+}
