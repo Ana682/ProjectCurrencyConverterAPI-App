4841eeef52bed34fa47880e534710d8055bc9cc4
Ana682 <queiroz.silva.ana@escola.pr.gov.br>
2025-08-22 11:57:39 -0300

    Indentifica o endpoint da API

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,8 +1,8 @@
 //Interface
 package com.example.projectcurrencyconverterapi;
 
-import retrofit2.call;
-import retrofit2.httpGET;
+import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface ExchangeRateService {
