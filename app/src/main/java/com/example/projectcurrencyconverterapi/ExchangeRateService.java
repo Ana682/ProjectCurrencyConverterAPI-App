ea10248f55d604cfcf6c5407cfb9a7a43112e4d1
Ana682 <queiroz.silva.ana@escola.pr.gov.br>
2025-09-03 08:54:58 -0300

    Adiciona as substituições

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,4 +1,5 @@
-//Interface
+//Interface que identifica o endpoint da API que será
+// Consumida pelo Retrofit
 package com.example.projectcurrencyconverterapi;
 
 import retrofit2.Call;
@@ -8,5 +9,14 @@ import retrofit2.http.Path;
 public interface ExchangeRateService {
     /**
      * Método para obter taxas de câmbio recentes
-     * **/
+     */
+
+    @GET("/vó/{apikey}/latest/{baseCurrency}")
+    Call<ExchangeRatesResponse> getExchangeRates(
+            @Path("apiKey") String apiKey,
+            //Substitui {apiKey} na URL com valor fornecido
+            @Path("baseCurrency") String baseCurrency
+
+            // Substitui {baseCurrency} na URL com valor fornecido
+    );
 }
--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -2,5 +2,18 @@
 //Json
 package com.example.projectcurrencyconverterapi;
 
+import java.util.HashMap; // Importa a cl
+import java.util.Map;
+
 public class ExchangeRatesResponse {
+    // declara um mapa que armazena taxas de conversão em moedas. (ex. USD, EUR, GBP)
+    // A chave (String) representa o código da Moeda e o valor (Double) representa a taxa de conversão.
+
+    private Map<String, Double> conversion_rates = new HashMap<>();
+    // Metodo publico para acessar as taxas de conversão
+    // Retorno o mapa de taxas de conversão
+
+    public Map<String, Double> getConversionRates() {
+        return conversion_rates;
+    }
 }
